87ef09ad11daa1731b7b29382c25890d77b009d1
Abby Wessels <abbywessels@quickenloans.com>
2018-12-13 14:22:10 -0500

    First Commit. Reworked with Identity and OWIN frameworks

---
+++ ASPLabs2/Controllers/AccountController.cs
@@ -0,0 +1,82 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using ASPLabs2.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+
+namespace ASPLabs2.Controllers
+{
+    public class AccountController : Controller
+    {
+        public UserManager<IdentityUser> UserManager => HttpContext.GetOwinContext().Get<UserManager<IdentityUser>>();
+
+        // GET: Account
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register(UserModel user)
+        {
+            if(ModelState.IsValid)
+            {
+                var identityResult = await UserManager.CreateAsync(new IdentityUser(user.username), (user.password));
+                if (identityResult.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", identityResult.Errors.FirstOrDefault());
+                    return View();
+                }
+            }
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User login)
+        {
+            if (ModelState.IsValid)
+            {
+                var userManager = HttpContext.GetOwinContext().Get<UserManager<IdentityUser>>();
+                var authManager = HttpContext.GetOwinContext().Authentication;
+
+                IdentityUser user = UserManager.Find(login.username, login.password);
+
+                //creates an identity(ident) with the user. This is the cookie that keeps the user logged in
+                if (user != null)
+                {
+                    var ident = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+                    //using our instance of authentication, we use the signin method
+                    //IsPersistent = false will logout the user and terminate that cookie (ident)
+                    authManager.SignIn(new AuthenticationProperties { IsPersistent = false }, ident);
+
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            ModelState.AddModelError("", "Invalid Username Or Password");
+            return View(login);
+
+                
+            
+        }
+    }
+}
---
+++ ASPLabs2/Controllers/DatabaseController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASPLabs2.Models;
+
+namespace ASPLabs2.Controllers
+{
+    public class DatabaseController : Controller
+    {
+        // GET: Database
+        [Authorize]
+        public ActionResult Index()
+        {
+            ASPLabs2Entities ORM = new ASPLabs2Entities();
+            
+            ViewBag.Users = ORM.Users.ToList();
+            // USER and USERS NOT WORKING ?
+
+            
+            return View();
+
+        }
+
+        public ActionResult AddUser(User newUser)
+        {
+            ASPLabs2Entities ORM = new ASPLabs2Entities();
+            ORM.Users.Add(newUser);
+            ORM.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteUser(int ID)
+        {
+            ASPLabs2Entities ORM = new ASPLabs2Entities();
+            User found = ORM.Users.Find(ID);
+            ORM.Users.Remove(found);
+            ORM.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult EditUser(int ID)
+        {
+            ASPLabs2Entities ORM = new ASPLabs2Entities();
+            User found = ORM.Users.Find(ID);
+            return View(found);
+        }
+
+        public ActionResult SaveChanges(User updatedUser)
+        {
+            ASPLabs2Entities ORM = new ASPLabs2Entities();
+            User oldUser = ORM.Users.Find(updatedUser.UserID);
+
+            oldUser.username = updatedUser.username;
+            oldUser.password = updatedUser.password;
+            oldUser.email = updatedUser.email;
+            oldUser.UserID = updatedUser.UserID;
+
+            ORM.Entry(oldUser).State = System.Data.Entity.EntityState.Modified;
+            ORM.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
+    }
+
+    
+}
